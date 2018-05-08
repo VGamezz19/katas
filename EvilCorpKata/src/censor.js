25791816658ddd99dd58f17b79698e1ed92af0be
VGamezz19 <victor.gamez1596@gmail.com>
2018-05-08 12:03:59 +0200

    array.join X

--- EvilCorpKata/src/censor.js
+++ EvilCorpKata/src/censor.js
@@ -1,17 +1,7 @@
 module.exports = (word, text) => {
-
   return text.replace(word, howManySymbolsToPut(word));
 };
 
 function howManySymbolsToPut(word) {
-  const length = word.length;
-  const symbol = "X";
-
-  let howMany = "";
-
-  for (let i = 0; i < length; i++) {
-    howMany += "X";
-  }
-  
-  return howMany;
+  return new Array(word.length + 1).join("X");
 }
