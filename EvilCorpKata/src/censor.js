e40ca04e361636be67449289f223bf5aadcdab3c
VGamezz19 <victor.gamez1596@gmail.com>
2018-05-09 21:45:27 +0200

    validating func

--- EvilCorpKata/src/censor.js
+++ EvilCorpKata/src/censor.js
@@ -1,6 +1,25 @@
 module.exports = (word, text) => {
+  try {
+
+    validate({ word, text })
+  } catch (err) {
+
+    console.error(err)
+  }
+
   return text.replace(word, howManySymbolsToPut(word));
 };
+
+function validate(obj) {
+  if (!obj instanceof Object) throw new Error("Invalid entry from validate(obj: Object)")
+
+  for (el of obj) {
+    if (!el) {
+
+      throw new Error("Mandatory propr -->" + el)
+    }
+  }
+}
 
 function howManySymbolsToPut(word) {
   return new Array(word.length + 1).join("X");
